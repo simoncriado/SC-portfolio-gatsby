aa019334008bd1c98e24246689ecf6e883c25e95
simon <scriado.f@gmail.com>
2022-05-31 12:30:35 +0200

    trying to fix relative path issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,18 +38,18 @@ module.exports = {
         plugins: [`gatsby-plugin-netlify-cms-paths`],
       },
     },
-    {
-      resolve: `gatsby-remark-relative-images-v2`,
-    },
-    {
-      resolve: `gatsby-remark-images`,
-      options: {
-        // It's important to specify the maxWidth (in pixels) of
-        // the content container as this plugin uses this as the
-        // base for generating different widths of each image.
-        maxWidth: 300,
-      },
-    },
+    // {
+    //   resolve: `gatsby-remark-relative-images-v2`,
+    // },
+    // {
+    //   resolve: `gatsby-remark-images`,
+    //   options: {
+    //     // It's important to specify the maxWidth (in pixels) of
+    //     // the content container as this plugin uses this as the
+    //     // base for generating different widths of each image.
+    //     maxWidth: 300,
+    //   },
+    // },
   ],
   siteMetadata: {
     title: "Simon Criado",
